22d8e162e6635a7b2acf0d51b195974ac0620b5b
τєαм ϰмαяτγ <85209355+S780821@users.noreply.github.com>
2021-11-10 22:52:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,6 @@ if os.path.exists(".env"):
     load_dotenv(".env")
     
 # Necessary Vars
-SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "Xmarty_Support"))
 API_ID = int(os.getenv("API_ID", "6"))
 API_HASH = os.getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
 SESSION = os.getenv("SESSION")
@@ -30,3 +29,4 @@ else:
 GRPPLAY = grp
 bot = Client(SESSION, API_ID, API_HASH, plugins=dict(root="XmartyBot"))
 call_py = PyTgCalls(bot)
+
